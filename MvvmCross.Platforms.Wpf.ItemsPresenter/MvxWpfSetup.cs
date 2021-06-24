4b4cc66fe5d79433c026b6266ce87c59a69c9ed3
Tony <tonymeissner70@gmail.com>
2021-06-24 10:48:07 +0200

    fixes

--- MvvmCross.Platforms.Wpf.ItemsPresenter/MvxWpfSetup.cs
+++ MvvmCross.Platforms.Wpf.ItemsPresenter/MvxWpfSetup.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using MvvmCross.Platforms.Wpf.Presenters;
+using MvvmCross.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
-using Microsoft.Extensions.Logging;
-using MvvmCross.Platforms.Wpf.Presenters;
-using MvvmCross.ViewModels;
 
 namespace MvvmCross.Platforms.Wpf.ItemsPresenter
 {
@@ -15,12 +15,14 @@ namespace MvvmCross.Platforms.Wpf.ItemsPresenter
     {
         protected override ILoggerFactory CreateLogFactory()
         {
-            throw new NotImplementedException();
+            return null;
+            //throw new NotImplementedException();
         }
 
         protected override ILoggerProvider CreateLogProvider()
         {
-            throw new NotImplementedException();
+            return null;
+            //throw new NotImplementedException();
         }
 
         protected override IMvxWpfViewPresenter CreateViewPresenter(ContentControl root)
